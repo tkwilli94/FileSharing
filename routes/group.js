8135d2e0043edf438d64e8f334a578c45d872e68
Eric Sorensen <soreric@byu.edu>
2016-03-22 18:31:05 +0000

    routes

---
+++ routes/group.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/createGroup', function(req,res,next) {
+
+
+});
+router.post('/addToGroup', function(req,res,next) {
+
+
+});
+router.post('/removeFromGroup', function(req,res,next) {
+
+
+});
+router.post('/deleteGroup', function(req,res,next) {
+
+
+});
+router.get('/getGroups', function(req,res,next) {
+
+});
+
+
+module.exports = router;
---
+++ routes/item.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/createItem', function(req,res,next) {
+
+
+});
+router.post('/updateItem', function(req,res,next) {
+
+
+});
+router.post('/deleteItem', function(req,res,next) {
+
+});
+router.post('/download', function(req,res,next) {
+
+});
+router.post('/release', function(req,res,next) {
+
+});
+
+
+router.get('/searchItems', function(req,res,next) {
+
+});
+
+
+module.exports = router;
