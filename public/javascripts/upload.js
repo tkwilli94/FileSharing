bacc600206d48d42a310686b5741a6ab5d3870cf
hunterrees <hunter.rees13@gmail.com>
2016-04-04 14:48:46 -0600

    Adding the form elements

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -2,6 +2,12 @@ angular.module('upload', [])
   .controller('uploadCtrl', [
     '$scope', '$http',
     function ($scope, $http) {
+
+  $scope.groupsData = [
+    {id : '0', name: 'CS360'},
+    {id : '1', name: 'CS340'}
+  ];
+  
   $scope.uploadFile = function(){
     console.log("hello world");
   }
