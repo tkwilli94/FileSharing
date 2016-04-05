3059825fe4e6becacaac4bc86b6c0acb0d573094
hunterrees <hunter.rees13@gmail.com>
2016-04-05 19:17:21 +0000

    Changing item.js to get the file db working

--- routes/item.js
+++ routes/item.js
@@ -1,14 +1,12 @@
 var fs = require('fs');
 var File = require('../models/file');
 
-fs.mkdir("./files");
-
 global.post_createItem  = function(req,res,next) {
    var newFile = new File();
-
+   console.log(req.body);
    newFile.filename = req.filename;
-   newFile.path = "../files/" + req.session.username + "/" +req.filename;
-   fs.mkdir(req.session.username);
+   newFile.path = "../files/" + req.session.passport.user + "/" +req.filename;
+   fs.mkdir("./files/" + req.session.passport.user, function(err){console.log(err)});
    newFile.location = req.location;
    newFile.copies = req.copies;
    newFile.save(function(err) {
@@ -17,10 +15,7 @@ global.post_createItem  = function(req,res,next) {
             throw err;
         }
         console.log('File Registration succesful');
-            return done(null, newFile);
     });
-   console.log(req.body);
-
 };
 global.post_updateItem  = function(req,res,next) {
   console.log(req.body);
