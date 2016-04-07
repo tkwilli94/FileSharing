c2080f9639d6b2cf26d136aec39293e4db1ab066
hunterrees <hunter.rees13@gmail.com>
2016-04-06 18:23:40 -0600

    Trying to upload

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -3,14 +3,9 @@ angular.module('upload', [])
     '$scope', '$http',
     function ($scope, $http) {
 
-  $scope.groupsData = [
-    {id : '0', name: 'CS360'},
-    {id : '1', name: 'CS340'}
-  ];
-
   $scope.files = [];
 
-  $scope.uploadFile = function(){
+  $scope.uploadFileData = function(){
     var file = $scope.fileToUpload;
     var data = JSON.stringify({"documentname" : $scope.documentName, "filename": $scope.fileName,
 	 "location": $scope.location, "copies":$scope.copies, "description":$scope.description});
@@ -18,10 +13,53 @@ angular.module('upload', [])
     $http.post('/createItem', data);
   }
 
+$scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+        var uploadUrl = "/updateItem";
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+
   $scope.getFiles = function(){
 	return $http.get('/searchItems').success(function(data){
         angular.copy(data, $scope.files);
   })};
 
+
+
   $scope.getFiles();
 }])
+
+upload.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+upload.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+          console.log("success");
+        })
+        .error(function(){
+          console.log("failure");
+        });
+    }
+}]);
