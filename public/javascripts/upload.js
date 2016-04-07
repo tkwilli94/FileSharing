f5a898a18fc0252ec325db6a47e53bc61ee861b8
hunterrees <hunter.rees13@gmail.com>
2016-04-07 12:01:42 -0600

    recreated upload.js

---
+++ public/javascripts/upload.js
@@ -0,0 +1,69 @@
+var uploadModule = angular.module('upload', []);
+
+uploadModule.controller('uploadCtrl', ['$scope', '$http', 'fileUpload', function ($scope, $http, fileUpload) {
+	$scope.files = [];
+	$scope.file = {};
+
+	$scope.uploadFileData = function(){
+	
+	}
+
+	$scope.uploadFile = function(){
+		var file = $scope.myFile;
+		console.dir(file);
+	var uploadUrl = " ";
+    fileUpload.uploadFileToUrl(file, uploadUrl);
+    var data = JSON.stringify({"documentname" : $scope.documentName, "filename": $scope.fileName,
+    "location": $scope.location, "copies":$scope.copies, "description":$scope.description});
+    console.log(data);
+    $http.post('/createItem', data);
+    $scope.getFiles();
+    };
+
+	$scope.getFiles = function(){
+		return $http.get('/searchItems').success(function(data){
+			angular.copy(data, $scope.files);
+		})
+	};
+	
+  $scope.download = function(file){
+    if(file.filename != "none"){
+      $http.get('/download', file, function(data){
+        var iframe = document.createElement("iframe");
+        iframe.setAttribute("src", retData.url);
+        iframe.setAttribute("style", "display: none");
+        document.body.appendChild(iframe);
+      });
+    }
+  }
+		
+	$scope.getFiles();
+}]);
+
+uploadModule.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+uploadModule.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+		fd.append('file', file);
+		
+		$http.post('/upload', fd, {
+			transformRequest: angular.identity,
+			headers: {'Content-Type': undefined}
+		})  
+    }
+}]);
